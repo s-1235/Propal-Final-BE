d3abaaf1cb9d23c48d79bbcdf591835d94f65edf
Mazen Jamshed <mazenjamshedmj@gmail.com>
2023-01-24 23:37:04 +0500

    admin controller

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,9 +1,13 @@
-const catchAsync = require("./../utils/catchAsync");
-const User = require("./../models/userModel");
+const catchAsync = require('./../utils/catchAsync');
+const User = require('./../models/userModel');
+const Contractor = require('../models/contractorModel');
+const Agency = require('../models/agencyModel');
 exports.adminData = catchAsync(async (req, res, next) => {
-  const users = await User.find({}).populate("properties");
+  const users = await User.find({}).populate('properties');
+  const contractors = await Contractor.find({});
+  const agencies = await Agency.find({});
   res.status(200).json({
-    status: "success",
-    adminData: { users },
+    status: 'success',
+    adminData: { users, contractors, agencies },
   });
 });
